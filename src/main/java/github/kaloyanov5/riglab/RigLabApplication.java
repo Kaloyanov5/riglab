989b9f942167427e73cf064c44cc377344ecae17
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2026-02-14 12:08:59 +0200

    Add Maven Wrapper and initial project setup

---
+++ src/main/java/github/kaloyanov5/riglab/RigLabApplication.java
@@ -0,0 +1,13 @@
+package github.kaloyanov5.riglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RigLabApplication.class, args);
+	}
+
+}
